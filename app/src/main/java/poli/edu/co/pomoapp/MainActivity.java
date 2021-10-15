0016cc4866e724b8b0c5a3a7bad9d57667084a0f
Frank Nicolas <frherran@poligran.edu.co>
2021-10-14 22:07:00 -0500

    Solución de errores

--- app/src/main/java/poli/edu/co/pomoapp/MainActivity.java
+++ app/src/main/java/poli/edu/co/pomoapp/MainActivity.java
@@ -41,7 +41,7 @@ public class MainActivity extends AppCompatActivity {
         DrawerLayout drawer = findViewById(R.id.drawer_layout);
         NavigationView navigationView = findViewById(R.id.nav_view);
         mAppBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow)
+                R.id.nav_home, R.id.nav_task, R.id.nav_report)
                 .setDrawerLayout(drawer)
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
